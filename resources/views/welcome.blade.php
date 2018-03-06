3340ab1937e54da14340558088a15fec2b1bdd05
Robin Arida <robin_arida@yahoo.com>
2018-03-06 10:26:24 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="/css/app.css" rel="stylesheet">
+
+    <!-- Styles -->
+</head>
+<body>
+    <section class="section">
+        <div class="container">
+          <h1 class="title">
+            Hello World
+        </h1>
+        <p class="subtitle">
+            My first website with <strong>Bulma</strong>!
+        </p>
+    </div>
+</section>
+</body>
+</html>
